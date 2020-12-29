ea14f60db0be0cc285b817c75b619cc88a4e8d66
ObjectNotFound <xml@live.com>
2020-12-29 19:33:32 +0800

    Update

--- console.sql
+++ console.sql
@@ -2,19 +2,43 @@ CREATE DATABASE ticket_sys;
 
 USE ticket_sys;
 
-CREATE TABLE `admin`(
-    `id` int not null primary key auto_increment,
-    `username` char(20) not null,
-    `password` char(60) not null
-);
-
 CREATE TABLE `user`(
-    `id` int not null key auto_increment,
-    `username` char(20) not null,
-    `password` char(60) not null,
-    `group_id` int
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `gid` INT,
+    `username` CHAR(20) NOT NULL,
+    `password` CHAR(60) NOT NULL,
+    `is_admin` TINYINT(1) NOT NULL
 );
 
-CREATE TABLE train(
-    
-)
+CREATE TABLE `train`(
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `train_name` CHAR(10) NOT NULL,
+    `start_station` CHAR(10) NOT NULL,
+    `start_time` DATETIME NOT NULL,
+    `stop_station` CHAR(10) NOT NULL,
+    `stop_time` DATETIME NOT NULL,
+    `capacity` INT NOT NULL,
+    `price` FLOAT NOT NULL CHECK(price >= 0.0)
+);
+
+CREATE TRIGGER `check_train` BEFORE INSERT ON `train` FOR EACH ROW
+BEGIN 
+		IF NEW.start_time < SYSDATE() or NEW.stop_time <= NEW.start_time THEN 
+				SIGNAL SQLSTATE "HY000" SET MESSAGE_TEXT = "Train time error.";
+		END IF;
+END;
+
+CREATE TABLE `order`(
+    `oid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `uid` INT NOT NULL,
+    FOREIGN KEY (`uid`) REFERENCES user(`id`),
+    `gid` INT,
+    `status` TINYINT(1) NOT NULL
+);
+
+CREATE TRIGGER `check_order` BEFORE INSERT ON `order` FOR EACH ROW
+BEGIN 
+    IF NEW.gid != NULL and NEW.gid NOT IN (SELECT DISTINCT `gid` from `user`) THEN
+        SIGNAL SQLSTATE "HY000" SET MESSAGE_TEXT = "Order group info error.";
+    END IF;
+END;
--- utils/createDB.py
+++ utils/createDB.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import sys
+
+from mysql.connector import cursor
 from . import errno
 
 def noDBConn(username: str, password: str, host: str, port: int):
@@ -13,11 +15,11 @@ def noDBConn(username: str, password: str, host: str, port: int):
 
 def createDB(Conn: mysql.connector.MySQLConnection, tableName: str):
     cursor = Conn.cursor()
-    dbList = cursor().execute("SHOW DATABASES;")
+    dbList = cursor.execute("SHOW DATABASES;")
     if tableName in dbList:
         print("[+] Drop the old database and create a new one.")
         try:
-            cursor().execute("DROP DATABASE %s", (tableName, ))
+            cursor.execute("DROP DATABASE %s", (tableName, ))
         except Exception:
             print("Drop database failed.")
             sys.exit(errno.DB_DROP_FAILED)
@@ -28,5 +30,56 @@ def createDB(Conn: mysql.connector.MySQLConnection, tableName: str):
         sys.exit(errno.DB_CREATE_FAILED)
 
 
-def createTable(Conn: mysql.connector.MySQLConnection):
-    pass
+def createTable(Conn: mysql.connector.MySQLConnection, tableName: str):
+    cursor = Conn.cursor()
+    try:
+        cursor.execute("USE DATABASE %s;", (tableName, ))
+        cursor.execute("""
+            CREATE TABLE `user`(
+                `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `gid` INT,
+                `username` CHAR(20) NOT NULL,
+                `password` CHAR(60) NOT NULL,
+                `is_admin` TINYINT(1) NOT NULL
+            );
+        """)
+        cursor.execute("""
+            CREATE TABLE `train`(
+                `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `train_name` CHAR(10) NOT NULL,
+                `start_station` CHAR(10) NOT NULL,
+                `start_time` DATETIME NOT NULL,
+                `stop_station` CHAR(10) NOT NULL,
+                `stop_time` DATETIME NOT NULL,
+                `capacity` INT NOT NULL,
+                `price` FLOAT NOT NULL CHECK(price >= 0.0)
+            );
+        """)
+        cursor.execute("""
+            CREATE TRIGGER `check_train` BEFORE INSERT ON `train` FOR EACH ROW
+            BEGIN 
+		        IF NEW.start_time < SYSDATE() or NEW.stop_time <= NEW.start_time THEN 
+				    SIGNAL SQLSTATE "HY000" SET MESSAGE_TEXT = "Train time error.";
+		        END IF;
+            END;
+        """)
+        cursor.execute("""
+            CREATE TABLE `order`(
+                `oid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `uid` INT NOT NULL,
+                FOREIGN KEY (`uid`) REFERENCES user(`id`),
+                `gid` INT,
+                `status` TINYINT(1) NOT NULL
+            );
+        """)
+        cursor.execute("""
+            CREATE TRIGGER `check_order` BEFORE INSERT ON `order` FOR EACH ROW
+            BEGIN 
+                IF NEW.gid != NULL and NEW.gid NOT IN (SELECT DISTINCT `gid` from `user`) THEN
+                    SIGNAL SQLSTATE "HY000" SET MESSAGE_TEXT = "Order group info error.";
+                END IF;
+            END;
+        """)
+    except Exception:
+        print("Create tables error.")
+        sys.exit()
