a7ccc3b67ad12506c1d49a4647cb533b8fe38d7b
ObjectNotFound <xml@live.com>
2021-01-04 19:53:43 +0800

    Signed-off-by: ObjectNotFound <xml@live.com>

--- routes/order.py
+++ routes/order.py
@@ -5,9 +5,21 @@ import utils.database
 
 @routes.route("/order")
 def order():
+    if "user" not in session or "is_admin" not in session:
+        return redirect("/login")
     return render_template("order.html")
+
+
+@routes.route("/placeOrder", methods=["POST"])
+def placeOrder():
+    if "user" not in session or "is_admin" not in session:
+        abort(401)
+    info = {}
+    return render_template("list_button.html", info=info)
 
 
 @routes.route("/refund")
 def refund():
+    if "user" not in session or "is_admin" not in session:
+        return redirect("/login")
     return render_template("refund.html")
